8b5c79aad66bf72a9d50a2e2b57ce3afe80fdf9b
dirj <dmitryantonov@hotmail.com>
2020-12-06 00:01:27 +0300

    I have changed output

--- src/main/java/ru/book/HelloWorld.java
+++ src/main/java/ru/book/HelloWorld.java
@@ -2,6 +2,6 @@ package ru.book;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! and to Dmitry");
+        System.out.println("Hello World! and to Dmitry and to Anatolii");
     }
 }
